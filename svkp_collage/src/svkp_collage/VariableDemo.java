bcbbb1ffece73332ec0a67b8a0788c49bcb3356b
santo <santo@10.0.0.108>
2024-11-06 13:50:42 +0530

    commit1

---
+++ svkp_collage/src/svkp_collage/VariableDemo.java
@@ -0,0 +1,16 @@
+package svkp_collage;
+
+public class VariableDemo {
+	int value=10;
+	void print()
+	{
+		String msg="hello";
+	  System.out.println(msg);
+	}
+	static String message="Hello students";  
+	public static void main(String[] args) {
+		
+
+	}
+
+}
