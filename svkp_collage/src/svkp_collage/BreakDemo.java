396fae02c6bbf1e3d61ba0a5ae503a715ee828e3
santo <santo@192.168.1.49>
2024-11-19 15:06:36 +0530

    third commit

---
+++ svkp_collage/src/svkp_collage/BreakDemo.java
@@ -0,0 +1,17 @@
+package svkp_collage;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<100;i++)
+		{
+			if(i>50)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ svkp_collage/src/svkp_collage/Built_in_packages.java
@@ -0,0 +1,14 @@
+package svkp_collage;
+public class Built_in_packages {
+
+	public static void main(String[] args) {
+		String greeting="hello World";
+		System.out.println("Original string " +greeting );
+		System.out.println("Uppercase string " + greeting.toUpperCase());
+		
+		int max=Math.max(19,30);//highest number
+		System.out.println(max);
+
+	}
+
+}
