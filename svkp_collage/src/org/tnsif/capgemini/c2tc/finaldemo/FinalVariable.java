f0a0751415983351b651b0ac7e356eb5ae74698b
santo <santo@192.168.1.28>
2025-04-01 15:24:10 +0530

    commit6

---
+++ svkp_collage/src/org/tnsif/capgemini/c2tc/finaldemo/FinalVariable.java
@@ -0,0 +1,12 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+		final int  mark_scored_leela=50;
+		mark_scored_leela=80; //final variable can't be changed
+		System.out.println(mark_scored_leela);
+
+	}
+
+}
---
+++ svkp_collage/src/org/tnsif/capgemini/c2tc/finaldemo/Finalclass.java
@@ -0,0 +1,28 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+final class calc //hema
+{
+	public void show()
+	{
+		System.out.println("AI integration done by hema");
+	}
+}
+
+class adav extends  calc //yasar
+{
+	public void show()
+	{
+		System.out.println("AI integration done by yasar");
+	}
+}
+
+
+
+public class Finalclass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
