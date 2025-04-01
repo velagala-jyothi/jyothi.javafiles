6dbf7ba2ca4828ca765eaa20b7d179069731e7f0
santo <santo@192.168.1.28>
2025-04-01 14:44:49 +0530

    commit4

---
+++ svkp_collage/src/org/tnsif/capgemini/c2tc/constructor/BeforeConstructor.java
@@ -0,0 +1,19 @@
+package org.tnsif.capgemini.c2tc.constructor;
+
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+
+
+public class BeforeConstructor {
+
+	public static void main(String[] args) {
+		Myclass obj=new Myclass();
+		System.out.println(obj.color + " " +obj.cost + " "+obj.Brand);
+
+	}
+
+}
